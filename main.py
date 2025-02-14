9f7075ef47339bdfc53321da749f8878ec78057f
BakedPotato <90150681+szybkirouterzyxel@users.noreply.github.com>
2025-02-14 17:30:12 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import platform
+cpu = os.cpu_count()
+cpuname = platform.machine()
+osver = platform.version()
+osname = platform.system()
+syspullver = "0.1"
+print(syspullver)
+print("============")
+print("CPU threads: ", cpu)
+print("CPU Architecture: ", cpuname)
+print("OS Name: ", osname)
+print("OS version:", osver)
+print("============")
